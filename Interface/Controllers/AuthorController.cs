83cf3d53c301b08b205ec46e73ebae27e27fd3df
LEGION <LEGION@X>
2025-10-16 02:35:34 +0400

    add tables

---
+++ Interface/Controllers/AuthorController.cs
@@ -0,0 +1,82 @@
+﻿using Application.Commands.AuthorCommands;
+using Application.Queries.AuthorQueries;
+using Application.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shared;
+using static Application.Commands.AuthorCommands.AddAuthorCommand;
+
+namespace Interface.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthorController : ControllerBase
+    {
+        private readonly ICommandExecutor _commandExecutor;
+        private readonly IQueryExecutor _queryExecutor;
+
+        public AuthorController(ICommandExecutor commandExecutor, IQueryExecutor queryExecutor)
+        {
+            _commandExecutor = commandExecutor;
+            _queryExecutor = queryExecutor;
+        }
+
+        #region Queries
+
+        [Route("GetAllAuthors")]
+        [HttpGet]
+        [Authorize(Roles = UserGroups.Admin)]
+        public async Task<QueryExecutionResult<GetAllAuthorsQueryResult>> GetAllAuthors([FromQuery] GetAllAuthorsQuery query) =>
+            await _queryExecutor.Execute<GetAllAuthorsQuery, GetAllAuthorsQueryResult>(query);
+
+        [Route("GetAuthorById")]
+        [HttpGet]
+        [Authorize(Roles = UserGroups.Admin)]
+        public async Task<QueryExecutionResult<AuthorQueryResultItem>> GetAuthorById([FromQuery] GetAuthorByIdQuery query) =>
+            await _queryExecutor.Execute<GetAuthorByIdQuery, AuthorQueryResultItem>(query);
+
+        #endregion
+
+        #region Commands
+
+        [Route("AddAuthor")]
+        [HttpPost]
+        [Authorize(Roles = UserGroups.Admin)]
+        public async Task<CommandExecutionResultGeneric<AuthorCommandResult>> AddAuthor([FromBody] AddAuthorCommand command) =>
+            await _commandExecutor.Execute(command);
+
+        [Route("UpdateAuthor")]
+        [HttpPut]
+        [Authorize(Roles = UserGroups.Admin)]
+        public async Task<CommandExecutionResultGeneric<AuthorCommandResult>> UpdateAuthor([FromBody] UpdateAuthorCommand command) =>
+            await _commandExecutor.Execute(command);
+        [Route("DeleteAuthor/{id}")]
+        [HttpDelete]
+        [Authorize(Roles = UserGroups.Admin)]
+        public async Task<CommandExecutionResult> DeleteAuthor([FromRoute] int id)
+        {
+            var command = new DeleteAuthorCommand { Id = id };
+            var result = await _commandExecutor.Execute(command);
+
+            // სწორად ვამოწმებთ result-ის წარმატებას
+            if (result.Success)
+            {
+                return new CommandExecutionResult
+                {
+                    Success = true
+                };
+            }
+            else
+            {
+                return new CommandExecutionResult
+                {
+                    Success = false,
+                    ErrorMessage = "წაშლა ვერ განხორციელდა"
+                };
+            }
+        }
+
+
+        #endregion
+    }
+}
---
+++ Interface/Controllers/ProductController.cs
@@ -0,0 +1,77 @@
+﻿using Application.Commands.ProductsCommands;
+using Application.Queries.ProductQueries;
+using Application.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shared;
+
+namespace Interface.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ICommandExecutor _commandExecutor;
+        private readonly IQueryExecutor _queryExecutor;
+
+        public ProductController(ICommandExecutor commandExecutor, IQueryExecutor queryExecutor)
+        {
+            _commandExecutor = commandExecutor;
+            _queryExecutor = queryExecutor;
+        }
+
+        #region Queries
+
+        [Route("GetAllProducts")]
+        [HttpGet]
+        [Authorize(Roles = UserGroups.All)]
+        public async Task<QueryExecutionResult<GetAllProductsQueryResult>> GetAllProducts([FromQuery] GetAllProductsQuery query) =>
+            await _queryExecutor.Execute<GetAllProductsQuery, GetAllProductsQueryResult>(query);
+
+        [Route("GetProductById/{id}")]
+        [HttpGet]
+        [Authorize(Roles = UserGroups.All)]
+        public async Task<QueryExecutionResult<ProductQueryResultItem>> GetProductById([FromRoute] int id) =>
+            await _queryExecutor.Execute<GetProductByIdQuery, ProductQueryResultItem>(new GetProductByIdQuery { Id = id });
+
+        #endregion
+
+        #region Commands
+
+        [Route("AddProduct")]
+        [HttpPost]
+        [Authorize(Roles = UserGroups.All)]
+        public async Task<CommandExecutionResultGeneric<ProductCommandResult>> AddProduct([FromBody] AddProductCommand command) =>
+            await _commandExecutor.Execute(command);
+
+        [Route("UpdateProduct")]
+        [HttpPut]
+        [Authorize(Roles = UserGroups.All)]
+        public async Task<CommandExecutionResultGeneric<ProductCommandResult>> UpdateProduct([FromBody] UpdateProductCommand command) =>
+            await _commandExecutor.Execute(command);
+
+        [Route("DeleteProduct/{id}")]
+        [HttpDelete]
+        [Authorize(Roles = UserGroups.Admin)]
+        public async Task<CommandExecutionResult> DeleteProduct([FromRoute] int id)
+        {
+            var command = new DeleteProductCommand { Id = id };
+            var result = await _commandExecutor.Execute(command);
+
+            if (result.Success)
+            {
+                return new CommandExecutionResult { Success = true };
+            }
+            else
+            {
+                return new CommandExecutionResult
+                {
+                    Success = false,
+                    ErrorMessage = "პროდუქტის წაშლა ვერ განხორციელდა"
+                };
+            }
+        }
+
+        #endregion
+    }
+}
